bb28e015fe0c8a757b209990ee74165d69b6bfca
jgk <james.invi@gmail.com>
2019-11-06 17:16:22 +0100

    added libraries, new GUI, added loot icons and arbitrary score system

--- Core.lua
+++ Core.lua
@@ -1,91 +1,200 @@
--- SLASH_RELOADUI1 ="/rl";
--- SlashCmdList.SLASH_RELOADUI=ReloadUI;
--- SLASH_FRAMESTK1="/fs"
--- SlashCmdList.FRAMESTK=fuction()
---   LoadAddOn("Blizzard_DebugTools");
---   FrameStackTooltip_Toggle()
--- end
-
--------------------------------------------------------
-
-
-local CheckerUI=CreateFrame("Frame","GearCheckerFrame",UIParent,"BasicFrameTemplateWithInset", "MovableTemplate");
-
-
-CheckerUI:SetSize(400,400);
-CheckerUI:SetPoint("Right");
-CheckerUI:SetMovable(true);
-
-CheckerUI:RegisterEvent("ITEM_PUSH");
-CheckerUI:RegisterEvent("BOSS_KILL");
-CheckerUI:RegisterEvent('GET_ITEM_INFO_RECEIVED')
-CheckerUI:RegisterEvent("CHAT_MSG_LOOT");
-CheckerUI:RegisterEvent("LOOT_OPENED");
-CheckerUI:RegisterEvent('PLAYER_ENTERING_WORLD')
-CheckerUI:RegisterEvent('TRADE_ACCEPT_UPDATE')
+local slots={
+INVTYPE_AMMO		= 0,
+INVTYPE_HEAD 		= 1, INVTYPE_FIRST_EQUIPPED = INVTYPE_HEAD,
+INVTYPE_NECK		= 2,
+INVTYPE_SHOULDER	= 3,
+INVTYPE_BODY		= 4,
+INVTYPE_CHEST		= 5,
+INVTYPE_WAIST		= 6,
+INVTYPE_LEGS		= 7,
+INVTYPE_FEET		= 8,
+INVTYPE_WRIST		= 9,
+INVTYPE_HAND		= 10,
+INVTYPE_FINGER1		= 11,
+INVTYPE_FINGER2		= 12,
+INVTYPE_TRINKET1	= 13,
+INVTYPE_TRINKET2	= 14,
+INVTYPE_BACK		= 15,
+INVTYPE_MAINHAND	= 16,
+INVTYPE_OFFHAND		= 17,
+INVTYPE_RANGED		= 18,
+INVTYPE_TABARD		= 19,
+};
 
 
+--IMPORTANT VARIABLES 
+local lootTable={};
+local f;
+local clearLoot;
+local scrollContainer;
+local scroll;
 
---TITLE
-CheckerUI.title=CheckerUI:CreateFontString(nil,"OVERLAY","GameFontHighlight");
-CheckerUI.title:SetPoint("LEFT",CheckerUI.TitleBg,"LEFT",5,0);
-CheckerUI.title:SetText("GearChecker");
+--LIBRARY STUFF
+CustomTooltip=CreateFrame("GameTooltip", "Tooltip", nil, "GameTooltipTemplate" );
+GearChecker = LibStub("AceAddon-3.0"):NewAddon("GearChecker", "AceConsole-3.0", "AceEvent-3.0");
 
-
--- CLOSE BUTTON
-CheckerUI.cancelButton=CreateFrame("Button",nil,CheckerUI,"GameMenuButtonTemplate");
-CheckerUI.cancelButton:SetPoint("BOTTOM",CheckerUI,"BOTTOM",0,15);
-CheckerUI.cancelButton:SetSize(120,30);
-CheckerUI.cancelButton:SetText("Close");
-CheckerUI.cancelButton:SetNormalFontObject("GameFontNormalLarge");
-
-
-lootButtons={};
-index=0;
-
-print(index);
+local AceGUI= LibStub("AceGUI-3.0");
+local icon = LibStub("LibDBIcon-1.0");
 
 
-MyGameTooltip=CreateFrame( "GameTooltip", "Tooltip", nil, "GameTooltipTemplate" ); -- Tooltip name cannot be nil
+local GearCheckerLDB = LibStub("LibDataBroker-1.1"):NewDataObject("GearChecker", {
+    type = "data source",
+    text = "GearChecker",
+    icon = "Interface\Icons\INV_Chest_Cloth_17",
+    OnClick = function() 
+        if f:IsShown() then
+            f:Hide();
+            f:ReleaseChildren();
+            collectgarbage("collect") -- Force garbage collection
 
-function eventHandler(self, event, ...)
-    if event=="CHAT_MSG_LOOT" then
-        local lootstring, _, _, _, player=...;
-        local itemLink = string.match(lootstring,"(|H(.+)|h)");
-        print(player .. " Got This: " .. itemLink);
-        --local item1 = GetInventoryItemLink("player", GetInventorySlotInfo("TRINKET0SLOT"));
-        --print(item1);
-        CreateLootIcon(itemLink,index);
-        --lootButton=CreateLootIcon(itemLink,index);
-        index=index+1;
-        print(index);
+        else
+            f:ReleaseChildren();
+            scroll:ReleaseChildren();
+            GearChecker:CreateBaseFrame();
+            RedrawButtons();
+            collectgarbage("collect") -- Force garbage collection
+
+        end
+    end,
+    })
+    
 
 
+function GearChecker:CreateBaseFrame()
+    -- Create a container frame
+    f = AceGUI:Create("Frame")
+    f:SetCallback("OnClose",function(widget) AceGUI:Release(widget) end)
+    f:SetTitle("GearChecker")
+    f:SetStatusText("Status Bar")
+    f:SetLayout("Flow")
+    
+    scrollContainer = AceGUI:Create("SimpleGroup");-- "InlineGroup" is also good
+    scrollContainer:SetFullWidth(true);
+    scrollContainer:SetFullHeight(true); -- probably?
+    scrollContainer:SetLayout("Fill"); -- important!
+
+
+    scroll = AceGUI:Create("ScrollFrame");
+    scroll:SetLayout("Flow"); -- probably?
+    scrollContainer:AddChild(scroll);
+
+
+    clearLoot=AceGUI:Create("Button");
+    clearLoot:SetText("ClearLoot");
+    clearLoot:SetCallback("OnClick", ClearLootTable);
+    dumpLoot=AceGUI:Create("Button");
+    dumpLoot:SetText("Dump Loot Table to chat");
+    dumpLoot:SetCallback("OnClick", function()             
+        for btn, val in ipairs(lootTable) do
+            GearCheck:Print(val.link);
+        end
+    end);
+    f:AddChild(clearLoot);
+    f:AddChild(dumpLoot);
+    f:AddChild(scrollContainer);
+
+
+
+end
+--ADDON EVENT HANDLERS
+function GearChecker:OnInitialize()
+    -- Called when the addon is loaded
+    GearChecker:RegisterEvent("CHAT_MSG_LOOT");
+    GearChecker:CreateBaseFrame();
+    self.db = LibStub("AceDB-3.0"):New("GearCheckerDB", { profile = { minimap = { hide = false, }, }, });
+    icon:Register("GearChecker", GearCheckerLDB, self.db.profile.minimap) self:RegisterChatCommand("gc", "OnChatCommand");
+end
+function GearChecker:OnChatCommand()
+    self.db.profile.minimap.hide = not self.db.profile.minimap.hide 
+    if self.db.profile.minimap.hide then 
+        icon:Hide("GearChecker") else icon:Show("GearChecker!") 
+    end 
+end
+function GearChecker:OnEnable()
+    -- Called when the addon is enabled
+end
+
+function GearChecker:OnDisable()
+    -- Called when the addon is disabled
+end
+
+
+
+--LOOT HANDLER, when somebody loots, request data then create a button
+function GearChecker:CHAT_MSG_LOOT(...)
+    local _, lootstring, player, _,player2 =...;
+    local itemLink = string.match(lootstring,"(|H(.+)|h)");
+    local _, ID = strsplit(":", itemLink);
+    local item = Item:CreateFromItemID(tonumber(ID));
+    
+    item:ContinueOnItemLoad(function()
+        local name, link, _, ilvl, _, _, _, _, itemSlot, texture=GetItemInfo(itemLink);
+        CreateButton(player,name,link,ilvl,itemSlot,texture,true);
+    end)
+end
+--clears the loot table and redraws all buttons
+function ClearLootTable()
+    scroll:ReleaseChildren();
+    GearChecker:Print("Cleared Loot Table");
+    for k in pairs (lootTable) do
+        lootTable [k] = nil
+    end
+    RedrawButtons();
+end
+--CREATES BUTTON and adds it to the frame, also handles tooltips
+function CreateButton(player, name, link, ilvl, itemSlot, texture, insertFlag)
+    local btn = AceGUI:Create("Icon");
+
+
+    btn:SetWidth(64);
+    btn:SetImage(texture);
+    btn:SetImageSize(64,64);
+    btn:SetLabel(player);
+    btn:SetCallback("OnClick", function()
+        local slot=slots[itemSlot];
+        GearChecker:Print("Item slot: ",slot);
+        GearChecker:Print("Currently Equipped: ",GetInventoryItemLink("player",slot));
+    end);
+    btn:SetCallback("OnEnter", function(self)
+        CustomTooltip:SetOwner(btn.frame, "ANCHOR_TOPRIGHT"); --Set our button as the tooltip's owner and attach it to the top right corner of the button.
+        CustomTooltip:SetHyperlink(link); --Set the tooltip's text using the default RGBA values (fully opaque gold text) with text wrapping on.
+        members= GetNumGroupMembers();
+        if members>0 then
+            for i=1,members do
+                local name, _, _, _, class=GetRaidRosterInfo(i);
+                CustomTooltip:AddLine(name ..  "score: " ..10);
+            end
+        else
+            CustomTooltip:AddLine("You are not in a group " .. UnitName("player") .. " :" .. 10);
+        end
+        CustomTooltip:Show(); --Show the tooltip
+    end);
+    btn:SetCallback("OnLeave", function(self)
+        CustomTooltip:Hide();
+    end)
+
+
+
+
+
+    scroll:AddChild(btn);
+    itemToStore={
+        player=player,
+        name=name,
+        link=link,
+        ilvl=ilvl,
+        itemSlot=itemSlot,
+        texture=texture
+    }
+    if(insertFlag==true) then 
+        table.insert(lootTable, itemToStore);
     end
 end
-function CreateLootIcon(item,index)
-    --ITEM DROPPED CREATE A BUTTON
-    local position=0+(index * 64);
-    droppedItem=CreateFrame("Button",nil, CheckerUI, "GameMenuButtonTemplate");
-    droppedItem:SetPoint("CENTER", CheckerUI, "CENTER", 0 + (index*64), (index*64));
-    droppedItem:SetSize(64,64);
-    droppedItem:SetNormalFontObject("GameFontNormalLarge");
-    local name, link, _, ilvl, _, _, _, _, _, texture=GetItemInfo(item);
-    droppedItem:SetNormalTexture(texture);
-    droppedItem:SetText(link);
-    droppedItem:SetScript("OnEnter", function(self)
-        MyGameTooltip:SetOwner(self, "ANCHOR_TOPRIGHT"); --Set our button as the tooltip's owner and attach it to the top right corner of the button.
-        MyGameTooltip:SetHyperlink(link); --Set the tooltip's text using the default RGBA values (fully opaque gold text) with text wrapping on.
-        MyGameTooltip:Show(); --Show the tooltip
-    end);
-    droppedItem:SetScript("OnLeave", function(self)
-        MyGameTooltip:Hide();
-    end)
-    return droppedItem;
-end
-function CreateButton(size,x,y,index)
 
+--REDRAWS ALL BUTTONS
+function RedrawButtons()
+    for btn, val in ipairs(lootTable) do
+        CreateButton(val.player, val.name, val.link, val.ilvl, val.itemSlot, val.texture,false);
+    end
 end
 
-
-CheckerUI:SetScript("OnEvent", eventHandler);
